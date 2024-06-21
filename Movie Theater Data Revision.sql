d1b53d9de949b76c60aa3c9c69e5a652a367a910
shiralip627 <164423590+shiralip627@users.noreply.github.com>
2024-06-21 11:17:34 -0400

    Add files via upload

---
+++ Movie Theater Data Revision.sql
@@ -0,0 +1,35 @@
+insert into Movie(
+	movie_id,
+	title,
+	rating
+)values(
+	12345,
+	'The Dark Knight',
+	'PG-13'
+);
+
+insert into Customer(
+	customer_id,
+	first_name,
+	last_name,
+	age
+)values(
+	789,
+	'Shelly',
+	'Woodridge',
+	28
+);
+
+insert into Concessions(
+	concession_id
+)values(
+	2
+);
+
+insert into Ticket(
+	ticket_id,
+	price
+)values(
+	456,
+	12.50
+);
